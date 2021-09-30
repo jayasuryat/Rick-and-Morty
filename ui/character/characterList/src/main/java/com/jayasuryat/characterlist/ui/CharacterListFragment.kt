ba51847d0a387908842eb314a509a068b435df1b
JayaSuryaT <jayasurya.thotapalli@biofourmis.com>
2021-09-30 08:38:27 +0530

    Minor package refactor of Characters related features

--- ui/character/characterList/src/main/java/com/jayasuryat/characterlist/ui/CharacterListFragment.kt
+++ ui/character/characterList/src/main/java/com/jayasuryat/characterlist/ui/CharacterListFragment.kt
@@ -1,6 +1,9 @@
 package com.jayasuryat.characterlist.ui
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.TranslateAnimation
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -31,6 +34,7 @@ class CharacterListFragment : BaseAbsFragment<CharacterListViewModel,
     override fun setupViews(): FragmentCharacterListBinding.() -> Unit = {
 
         binding.clRoot.post(::revealRoot)
+        animateViews()
 
         ivBack.shrinkOnClick { findNavController().popBackStack() }
 
@@ -64,5 +68,35 @@ class CharacterListFragment : BaseAbsFragment<CharacterListViewModel,
             .build()
             .animation
             .start()
+    }
+
+    private fun animateViews() {
+
+        val animDuration: Long = 300
+        val animInterpolator = DecelerateInterpolator()
+
+        ObjectAnimator.ofFloat(binding.ivBack, "alpha", 0f, 0.0f, 1f)
+            .apply {
+                duration = animDuration
+                interpolator = animInterpolator
+            }.start()
+
+        ObjectAnimator.ofFloat(binding.rvCharactersList, "alpha", 0f, 0.0f, 1f)
+            .apply {
+                duration = animDuration
+                interpolator = animInterpolator
+            }.start()
+
+        TranslateAnimation(-100f, 0f, 0f, 0f)
+            .apply {
+                duration = animDuration
+                interpolator = animInterpolator
+            }.run { binding.ivBack.startAnimation(this) }
+
+        TranslateAnimation(0f, 0f, 200f, 0f)
+            .apply {
+                duration = animDuration
+                interpolator = animInterpolator
+            }.run { binding.rvCharactersList.startAnimation(this) }
     }
 }
