dd8f250f1ca721219fa8625341bc33101a9ee343
JayaSuryaT <jayasurya.dc@gmail.com>
2021-10-30 16:09:41 +0530

    Add `JC` to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,30 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
     // Test
     testImplementation(Dependency.Test.junit)
     androidTestImplementation(Dependency.Test.androidJunit)
@@ -75,4 +95,14 @@ dependencies {
     implementation(project(Dependency.Module.episodeDetails))
     implementation(project(Dependency.Module.locationList))
     implementation(project(Dependency.Module.locationDetils))
+
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.compose.material:material:1.0.4")
+    implementation("androidx.compose.animation:animation:1.0.4")
+    implementation("androidx.compose.ui:ui-tooling:1.0.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.4")
 }
