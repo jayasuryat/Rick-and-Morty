d3169a3b79cad412e3a141c9c7bc064a29502be7
JayaSuryaT <jayasurya.dc@gmail.com>
2021-11-02 23:19:25 +0530

    Add versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
 
-    val compose_version by extra("1.0.1")
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 
     dependencies {
@@ -13,9 +13,14 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.39.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
+}
+
+apply {
+    from("buildScripts/versionsPlugin.gradle")
 }
 
 tasks.register("clean", Delete::class) {
