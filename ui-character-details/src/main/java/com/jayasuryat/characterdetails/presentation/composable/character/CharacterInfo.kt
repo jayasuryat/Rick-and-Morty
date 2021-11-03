cdf976a8f9b32d753d8758bfbe8195ab8ab5d847
JayaSuryaT <jayasurya.dc@gmail.com>
2021-11-03 20:35:45 +0530

    Fix character info icons

--- ui-character-details/src/main/java/com/jayasuryat/characterdetails/presentation/composable/character/CharacterInfo.kt
+++ ui-character-details/src/main/java/com/jayasuryat/characterdetails/presentation/composable/character/CharacterInfo.kt
@@ -1,25 +1,33 @@
 package com.jayasuryat.characterdetails.presentation.composable.character
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.IdRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jayasuryat.characterdetails.R
 import com.jayasuryat.characterdetails.domain.models.CharacterDetails
+import com.jayasuryat.characterdetails.domain.models.CharacterDetails.Gender.*
+import com.jayasuryat.characterdetails.domain.models.CharacterDetails.Species
+import com.jayasuryat.characterdetails.domain.models.CharacterDetails.Status.Alive
+import com.jayasuryat.characterdetails.domain.models.CharacterDetails.Status.Dead
 
 
 @Composable
@@ -111,32 +119,120 @@ private fun CharacterInfoBottom(
             .wrapContentWidth(),
     ) {
 
-        Icon(
-            modifier = Modifier.size(30.dp),
-            painter = painterResource(id = R.drawable.icon_alien),
-            contentDescription = "Species type ${character.species.value}",
-            tint = MaterialTheme.colors.background,
+        @IdRes val speciesImageId = when (character.species) {
+            Species.Alien -> R.drawable.icon_alien
+            Species.Human -> R.drawable.icon_user
+            Species.Humanoid -> R.drawable.icon_robo
+            Species.Other -> R.drawable.icon_paw
+        }
+
+        @IdRes val genderImageId = when (character.gender) {
+            Female -> R.drawable.icon_gender_female
+            Male -> R.drawable.icon_gender_male
+            Genderless -> R.drawable.icon_gender_neutral
+            Unknown -> R.drawable.icon_gender_neutral // TODO Need to add a diff icon
+        }
+
+        @IdRes val statusColorId = when (character.status) {
+            Alive -> R.color.green
+            Dead -> R.color.red
+            CharacterDetails.Status.Unknown -> R.color.grey
+        }
+
+        InfoIcon(
+            value = character.species.value,
+            icon = speciesImageId,
         )
 
-        Spacer(modifier = Modifier.width(32.dp))
+        Spacer(modifier = Modifier.width(8.dp))
 
-        Icon(
-            modifier = Modifier.size(30.dp),
-            painter = painterResource(id = R.drawable.icon_alien),
-            contentDescription = "Species type ${character.species.value}",
-            tint = MaterialTheme.colors.background,
+        StatusIcon(
+            value = character.status.value,
+            statusColor = colorResource(statusColorId),
         )
 
-        Spacer(modifier = Modifier.width(32.dp))
+        Spacer(modifier = Modifier.width(8.dp))
 
-        Icon(
-            modifier = Modifier.size(30.dp),
-            painter = painterResource(id = R.drawable.icon_gender_neutral),
-            contentDescription = "Species type ${character.species.value}",
-            tint = MaterialTheme.colors.background,
+        InfoIcon(
+            value = character.gender.value,
+            icon = genderImageId,
         )
     }
 }
+
+@Composable
+private fun InfoIcon(
+    value: String,
+    @DrawableRes icon: Int,
+    modifier: Modifier = Modifier,
+) {
+
+    Column(
+        modifier = modifier
+            .wrapContentSize()
+            .width(60.dp),
+    ) {
+
+        Icon(
+            modifier = Modifier
+                .size(30.dp)
+                .align(CenterHorizontally),
+            painter = painterResource(id = icon),
+            contentDescription = "$value icon",
+            tint = MaterialTheme.colors.onPrimary,
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            text = value,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .wrapContentSize()
+                .align(CenterHorizontally),
+            color = MaterialTheme.colors.onPrimary.copy(alpha = 0.5f),
+            style = MaterialTheme.typography.body2.copy(fontSize = 12.sp),
+        )
+    }
+}
+
+@Composable
+private fun StatusIcon(
+    value: String,
+    statusColor: Color,
+    modifier: Modifier = Modifier,
+) {
+
+    Column(
+        modifier = modifier
+            .wrapContentSize()
+            .width(60.dp),
+    ) {
+
+        Surface(
+            modifier = Modifier
+                .size(30.dp)
+                .clip(CircleShape)
+                .align(CenterHorizontally),
+            color = statusColor
+        ) {}
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            text = value,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .wrapContentSize()
+                .align(CenterHorizontally),
+            color = MaterialTheme.colors.onPrimary.copy(alpha = 0.5f),
+            style = MaterialTheme.typography.body2.copy(fontSize = 12.sp),
+        )
+    }
+}
+
 
 @Preview
 @Composable
@@ -145,30 +241,6 @@ private fun PrevCharacter_Info(
 ) {
 
     CharacterInfo(
-        character = character,
-    )
-}
-
-@Preview
-@Composable
-private fun PrevCharacter_Info_Top(
-    @PreviewParameter(CharacterParameterProvider::class) character: CharacterDetails,
-) {
-
-    Column {
-        CharacterInfoTop(
-            character = character,
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun PrevCharacter_Info_Bottom(
-    @PreviewParameter(CharacterParameterProvider::class) character: CharacterDetails,
-) {
-
-    CharacterInfoBottom(
         character = character,
     )
 }
