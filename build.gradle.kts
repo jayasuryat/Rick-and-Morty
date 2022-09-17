a616ac07e9563b75289ee7f9a54f2ce1bf90850f
JayaSuryaT <jayasurya.dc@gmail.com>
2022-09-17 16:08:38 +0530

    Bump up dependencies version and consume api changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.2")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.39.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
